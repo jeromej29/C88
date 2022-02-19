73b1cd91f3f2303f556555525ba4ef762e8e4325
jeromej29 <75542095+jeromej29@users.noreply.github.com>
2022-02-19 11:00:47 -0700

    Add files via upload

---
+++ spectagram-stage-8-main/spectagram-stage-8-main/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0SQ-O5Vf4BmMarZXSLNBzcXdhSnxX81I",
+    authDomain: "pro-c88-879bc.firebaseapp.com",
+    databaseURL: "https://pro-c88-879bc-default-rtdb.firebaseio.com",
+    projectId: "pro-c88-879bc",
+    storageBucket: "pro-c88-879bc.appspot.com",
+    messagingSenderId: "856633253181",
+    appId: "1:856633253181:web:4b73651b1932849e4b3954"
+  };
